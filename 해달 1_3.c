5317d3665bbaad46b7ec8631da81a0832b775b25
510james <510james@naver.com>
2025-09-29 18:48:22 +0900

    Add files via upload

---
+++ 해달 1_3.c
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 6;
+	int b = 4;
+	int c = 8;
+	printf("%d\n", (a + b) / c);
+	printf("%d\n", ((a / c) + (b / c)) / c);
+	printf("%d\n", (a * b) / c);
+	printf("%d\n", ((a / c) * (b / c)) / c);
+}
